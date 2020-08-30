fe80f1f8d1b5afa24a92b053d842f945cdd32579
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-08-30 22:39:29 +0900

    Updates

---
+++ precache-manifest.71a53b03bd62be586888db5d3179ed06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603205ef1c9d9336edf290eb156c0042",
+    "url": "/sangstagrams/index.html"
+  },
+  {
+    "revision": "9f5740ab0ba62536fd62",
+    "url": "/sangstagrams/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "9f5740ab0ba62536fd62",
+    "url": "/sangstagrams/static/js/2.d47661da.chunk.js"
+  },
+  {
+    "revision": "843b1e6461bdc31a2f60595444704a94",
+    "url": "/sangstagrams/static/js/2.d47661da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db108d5f485e3df4ead",
+    "url": "/sangstagrams/static/js/main.bae63ff2.chunk.js"
+  },
+  {
+    "revision": "21049d38e94b3076782a",
+    "url": "/sangstagrams/static/js/runtime-main.42250ee9.js"
+  },
+  {
+    "revision": "d309b6b76780cf5712cc1258fe61125a",
+    "url": "/sangstagrams/static/media/logo.d309b6b7.png"
+  }
+]);
